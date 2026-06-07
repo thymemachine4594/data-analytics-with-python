515a947a8161c092d692c886e10068f829f1969e
Jennifer Shiby <161931141+thymemachine4594@users.noreply.github.com>
2026-06-07 12:55:01 +0530

    Delete classeg1.py

--- classeg1.py
+++
@@ -1,7 +0,0 @@
-class python_advance_da:
-    def niru(self):
-        print("niru, CS department")
-    def jenna(self):
-        print("jenna, CS department")
-    def achu(self):
-        print('achu,AI department')
