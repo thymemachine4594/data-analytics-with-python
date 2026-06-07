f2cc5184b0bf16a715739a534e1abc51c7ba1730
Jennifer Shiby <161931141+thymemachine4594@users.noreply.github.com>
2026-06-07 12:56:09 +0530

    Delete exceptionhandling.py

--- exceptionhandling.py
+++
@@ -1,9 +0,0 @@
-try:
-    num=int(input("Enter a number: "))
-    print("number: ", num)
-except ValueError:
-    print("Invalid input. Please enter a valid integer.")
-else:
-    print("You entered a valid number.")
-finally:
-    print("Execution completed.")
